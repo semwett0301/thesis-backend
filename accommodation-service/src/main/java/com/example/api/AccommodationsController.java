945bb85e9377e4871c3402d7f9b1071f6ab9d2b4
semen.mokrov <semen.mokrov@gmail.com>
2024-05-16 01:10:18 +0300

    feat: add controller to accommodation service

--- accommodation-service/src/main/java/com/example/api/AccommodationsController.java
+++ accommodation-service/src/main/java/com/example/api/AccommodationsController.java
@@ -1,8 +1,7 @@
 package com.example.api;
 
 import com.example.City;
-import com.example.model.dto.request.TicketEnum;
-import com.example.model.dto.response.TicketResponse;
+import com.example.model.dto.response.AccommodationResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +11,9 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-public class TicketsController {
+public class AccommodationsController {
     @GetMapping
-    public List<TicketResponse> getTickets(
-            @RequestParam(name = "type") TicketEnum ticketType,
+    public List<AccommodationResponse> getAccommodations(
             @RequestParam(name = "start_date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
             @RequestParam(name = "end_date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate,
             @RequestParam(name = "max_price") Integer maxPrice,
---
+++ accommodation-service/src/main/java/com/example/model/dto/response/AccommodationResponse.java
@@ -0,0 +1,16 @@
+package com.example.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AccommodationResponse {
+    private Integer price;
+    private Integer center_distance;
+    private String name;
+    private String link;
+    private String source;
+    private Integer rating;
+    private Integer reviews_amount;
+}
